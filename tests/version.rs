db65f264672ec5eaa30789842fe0ed2d054d6fb3
hhatto <hhatto.jp@gmail.com>
2018-03-11 00:06:09 +0900

    fix brew,grep command

--- tests/version.rs
+++ tests/version.rs
@@ -9,18 +9,28 @@ use curl::Version;
 fn check_homebrews_curl() {
     let brew = Command::new("brew").arg("list").stdout(Stdio::piped()).spawn();
     let use_homebrew = match brew {
-        Ok(_) => {
-            let grep = Command::new("grep")
+        Ok(mut b) => {
+            let mut grep = Command::new("grep")
                 .args(&["-w", "curl"])
                 .stdin(Stdio::piped())
-                .output()
+                .spawn()
                 .unwrap();
-            match grep.status.code() {
+            {
+                let out = b.stdout.as_mut().unwrap();
+                let in_ = grep.stdin.as_mut().unwrap();
+                std::io::copy(out, in_).unwrap();
+            }
+            let _brew_status = b.wait().unwrap();
+            let grep_status = grep.wait().unwrap();
+            match grep_status.code() {
                 Some(0) => true,
                 _ => false,
             }
         },
-        Err(_) => false,
+        Err(e) => {
+            println!("fail brew command: {:?}", e);
+            false
+        },
     };
     let v = Version::get();
     if use_homebrew {
@@ -29,6 +39,5 @@ fn check_homebrews_curl() {
     } else {
         // bundled libcurl is 7.54.0 on macos10.12 and macos10.13
         assert!(v.version() == "7.54.0");
-        assert_eq!(v.version(), "7.58.0");
     }
 }
